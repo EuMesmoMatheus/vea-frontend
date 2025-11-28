3464c7d0c973467cb53cdbc5da3ed6672b6f646d
EuMesmoMatias <teteuzinhovlogs@gmai.com>
2025-11-28 16:58:16 -0300

    ajuster no envoimnets

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:63561/api' // URL do backend; ajuste a porta se diferente
+  production: true,
+  apiUrl: 'https://vea-api.up.railway.app' 
 };
